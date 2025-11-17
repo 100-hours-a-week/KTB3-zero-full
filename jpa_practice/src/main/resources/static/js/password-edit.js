21b877bba6e7f8aa3bc26bf3b6c9cb6b3c1d9158
minzer01 <minzero@ewhain.net>
2025-11-17 17:47:29 +0900

    html, css, js 추가

---
+++ jpa_practice/src/main/resources/static/js/password-edit.js
@@ -0,0 +1,7 @@
+document.getElementById('btnEdit').addEventListener('click', () => {
+  alert('요소가 클릭되었습니다!');
+});
+
+const editTitle = document.getElementById('pw-new').value;
+const editContent = document.getElementById('pw-confirm').value;
+
---
+++ jpa_practice/src/main/resources/static/js/post-new.js
@@ -0,0 +1,7 @@
+document.getElementById('btnSubmit').addEventListener('click', () => {
+  alert('요소가 클릭되었습니다!');
+});
+
+const newTitle = document.getElementById('new-title').value;
+const newContent = document.getElementById('new-content').value;
+
